0deaf6f5ec84241128e2ee75ca2b69cd8ec3e04e
Gregor Krmelj <gregor@krmelj.xyz>
2022-09-04 22:31:54 +0200

    fix: remove unecessary tests and fix logic error

--- internal/client.go
+++ internal/client.go
@@ -59,7 +59,7 @@ func RequestRoutine(p RequestRoutineParameters, cs crypto.CipherSuite, opt Reque
 
 	s := fmt.Sprintf("OpenSPA sending request for access to target (%s %s/%d)",
 		rd.TargetIP, rd.TargetProtocol, rd.TargetPortStart)
-	if rd.TargetPortEnd != rd.TargetPortEnd {
+	if rd.TargetPortStart != rd.TargetPortEnd {
 		s = fmt.Sprintf("%s-%d", s, rd.TargetPortEnd)
 	}
 
