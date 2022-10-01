4b08f28b4b4a74b06234c2c5c6f48838257c99f7
Gregor Krmelj <gregor@krmelj.xyz>
2022-10-01 22:31:26 +0200

    feat: add cli command adk proof

--- internal/cmd/adk.go
+++ internal/cmd/adk.go
@@ -22,8 +22,17 @@ var ADKSecretCmd = &cobra.Command{
 	PreRun: PreRunLogSetupFn,
 }
 
+var ADKProofCmd = &cobra.Command{
+	Use:    "proof [ADK secret]",
+	Short:  "Generate ADK proof",
+	Run:    adkProofCmdRunFn,
+	PreRun: PreRunLogSetupFn,
+	Args:   cobra.ExactArgs(1),
+}
+
 func ADKCmdSetup(c *cobra.Command) {
 	c.AddCommand(ADKSecretCmd)
+	c.AddCommand(ADKProofCmd)
 }
 
 func adkCmdRunFn(cmd *cobra.Command, args []string) {
@@ -37,4 +46,20 @@ func adkSecretCmdRunFn(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 	fmt.Fprintf(os.Stdout, "Secret: %s\n", secret)
+}
+
+func adkProofCmdRunFn(cmd *cobra.Command, args []string) {
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "invalid number of arguments\n")
+		os.Exit(1)
+	}
+
+	secret := args[0]
+
+	proof, err := openspalib.ADKGenerateProof(secret)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to generate ADK proof, err: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stdout, "Proof: %d\n", proof)
 }
